7e996eae6de4f77473a39642c4dc2a275cd58158
reillyrichards <reillyrichardswork@gmail.com>
2026-05-12 15:10:09 +0100

    Add logger

---
+++ logger.py
@@ -0,0 +1,6 @@
+#File handles all outputs, prints to terminal and log file.
+#Isolated incase HOW we want to change logged, only this file would need changing.
+
+import logging
+import os
+from config import LOG_FILE
