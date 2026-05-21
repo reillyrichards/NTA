993e76bab2e5e46d87905997aa665e6626cfcb56
reillyrichards <reillyrichardswork@gmail.com>
2026-05-21 19:10:26 +0100

    Refactor packet processing logic in analyser module to enhance threat detection capabilities

--- analyser.py
+++ analyser.py
@@ -6,3 +6,34 @@ from scapy.all import sniff, IP, TCP, UDP
 from detector import ThreatDetector
 from logger import log_info
 from config import INTERFACE
+
+detector = ThreatDetector()
+
+def process_packet(packet):
+    """
+    Scapy calls this automatically for every captured packet. 
+    Used to extract source IP and destination port and pass them along.
+    """
+
+
+    #haslayer(IP) checks if packet contains an IP header
+    #If it does not contain one, no point in processing since we can't attribute
+    #traffic to anyone
+    if not packet.haslayer(IP):
+        return
+    
+    #packet[IP].src gives us source IP address as a string
+    src_ip = packet[IP].src
+    
+    dst_port = None
+
+    if packet.haslayer(TCP):
+        dst_port = packet[TCP].dport
+    elif packet.haslayer(UDP):
+        dst_port = packet[UDP].dport
+
+    if dst_port is None:
+        return
+
+    #Detector only needs IP and port
+    detector.analyse_packet(src_ip,dst_port) 
